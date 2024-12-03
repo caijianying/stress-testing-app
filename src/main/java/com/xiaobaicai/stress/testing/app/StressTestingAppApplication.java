08a4732f5f379a1f184c7eb9b0a3b9ac6a4c5027
caijianying <956203814@qq.com>
2024-12-03 15:05:14 +0800

    Add files via upload
    init

---
+++ src/main/java/com/xiaobaicai/stress/testing/app/StressTestingAppApplication.java
@@ -0,0 +1,13 @@
+package com.xiaobaicai.stress.testing.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StressTestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StressTestingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaobaicai/stress/testing/app/StressTestingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaobaicai.stress.testing.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StressTestingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
